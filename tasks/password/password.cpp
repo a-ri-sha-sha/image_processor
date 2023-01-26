6449f5af7ae1651410d206541a5c00380b9ba469
Arina Shaydeman <adshaydeman_1@edu.hse.ru>
2023-01-26 12:12:28 +0300

    password

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -1,5 +1,37 @@
 #include "password.h"
-
+const int MIN_LEN = 8;
+const int MAX_LEN = 14;
+const int MIN_ASCII = 33;
+const int MAX_ASCII = 126;
 bool ValidatePassword(const std::string& password) {
-    return false;
+    bool big_let = false;
+    bool small_let = false;
+    bool num = false;
+    bool symb = false;
+    if (password.size() < MIN_LEN || password.size() > MAX_LEN) {
+        return false;
+    }
+    for (int i = 0; i < password.size(); ++i) {
+        char elem = password[i];
+        if (elem < MIN_ASCII || elem > MAX_ASCII) {
+            return false;
+        }
+        if (islower(elem)) {
+            small_let = true;
+        } else {
+            if (isupper(elem)) {
+                big_let = true;
+            } else {
+                if (isdigit(elem)) {
+                    num = true;
+                } else {
+                    symb = true;
+                }
+            }
+        }
+    }
+    if (big_let + small_let + num + symb < 3) {
+        return false;
+    }
+    return true;
 }
