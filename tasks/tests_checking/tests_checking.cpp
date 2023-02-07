f437fefd3b7d3b50d7da7c3921f8fb208a71028a
Arina Shaydeman <adshaydeman_1@edu.hse.ru>
2023-02-07 12:14:55 +0300

    tests_checking git commit -m tests_checking
    fhfgfg
    ''

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,6 +1,19 @@
+#include <deque>
 #include "tests_checking.h"
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    return {};
+    std::deque<std::string> deq;
+    for (auto& action : student_actions) {
+        if (action.side == Side::Top) {
+            deq.push_front(action.name);
+        } else {
+            deq.push_back(action.name);
+        }
+    }
+    std::vector<std::string> ans;
+    for (size_t i : queries) {
+        ans.push_back(deq.at(i - 1));
+    }
+    return ans;
 }
