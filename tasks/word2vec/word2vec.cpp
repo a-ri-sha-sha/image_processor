12da0c2ce962d30acbd4f2fae14d4f0e50915ec9
Arina Shaydeman <adshaydeman_1@edu.hse.ru>
2023-02-08 20:08:53 +0300

    word2vec cringe3

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -1,11 +1,12 @@
 #include "word2vec.h"
 
 #include <vector>
+#include <limits>
 
 std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
                                           const std::vector<std::vector<int>>& vectors) {
     std::vector<std::string> ans;
-    int64_t max_len = 0;
+    int64_t max_len = std::numeric_limits<int64_t>::min();
     int64_t len = 0;
     for (size_t i = 1; i < vectors.size(); ++i) {
         for (size_t j = 0; j < vectors[i].size(); ++j) {
