11b6a70dd341055d18d656a364352bcd6a253412
Arina Shaydeman <adshaydeman_1@edu.hse.ru>
2023-02-08 20:00:08 +0300

    word2vec cringe2

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -9,7 +9,7 @@ std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
     int64_t len = 0;
     for (size_t i = 1; i < vectors.size(); ++i) {
         for (size_t j = 0; j < vectors[i].size(); ++j) {
-            len += static_cast<int64_t>(vectors[i][j]) * static_cast<int64_t>(vectors[0][i]);
+            len += static_cast<int64_t>(vectors[i][j]) * static_cast<int64_t>(vectors[0][j]);
         }
         if (len == max_len) {
             ans.push_back(words[i]);
