eca75ffdc403e66b41aa115ff4eda3dd5a41feae
Arina Shaydeman <adshaydeman_1@edu.hse.ru>
2023-02-09 15:05:46 +0300

    scorer

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -1,5 +1,38 @@
 #include "scorer.h"
+#include <tuple>
+
+bool Comp(const Event* lhv, const Event* rhv) {
+    return lhv->time < rhv->time;
+}
 
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
-    return {}; // test
+    ScoreTable ans;
+    std::vector<const Event*> event;
+    std::map<StudentName, std::map<TaskName, std::tuple<bool, bool>>> table;
+    std::set<StudentName> students;
+    std::set<TaskName> task;
+    for (const Event& ev : events) {
+        if (ev.time <= score_time) {
+            event.push_back(&ev);
+            students.insert(ev.student_name);
+            task.insert(ev.task_name);
+        }
+    }
+    std::sort(event.begin(), event.end(), Comp);
+    for (const Event* ev : event) {
+        if (ev->event_type == EventType::CheckSuccess) {
+            std::get<0>(table[ev->student_name][ev->task_name]) = true;
+        }
+        if (ev->event_type == EventType::MergeRequestOpen) {
+            std::get<1>(table[ev->student_name][ev->task_name]) = true;
+        }
+    }
+    for (StudentName stu : students) {
+        for (TaskName taskName : task) {
+            if (std::get<0>(table[stu][taskName]) and !(std::get<1>(table[stu][taskName]))) {
+                ans[stu].insert(taskName);
+            }
+        }
+    }
+    return ans;
 }
