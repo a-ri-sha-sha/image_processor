87553fdba2bdec8a0faae4d85a8105c378d1ac51
Arina Shaydeman <adshaydeman_1@edu.hse.ru>
2023-03-01 10:38:25 +0300

    attempt1

--- tasks/mines/minesweeper.h
+++ tasks/mines/minesweeper.h
@@ -17,10 +17,26 @@ public:
         DEFEAT,
     };
 
+    enum class ClosedCellType {
+        Mine,
+        Neighbour,
+        Empty,
+    };
+
+    enum class OpenCellType {
+        Mine,
+        Flag,
+        Neighbour,
+        Closed,
+    };
+
     using RenderedField = std::vector<std::string>;
+    using VectorNeighbour = std::vector<Cell>;
 
     Minesweeper(size_t width, size_t height, size_t mines_count);
     Minesweeper(size_t width, size_t height, const std::vector<Cell>& cells_with_mines);
+
+    void NearbyMine(size_t x, size_t y);
 
     void NewGame(size_t width, size_t height, size_t mines_count);
     void NewGame(size_t width, size_t height, const std::vector<Cell>& cells_with_mines);
@@ -28,8 +44,18 @@ public:
     void OpenCell(const Cell& cell);
     void MarkCell(const Cell& cell);
 
+    VectorNeighbour MakeVectorNeig(size_t x, size_t y);
     GameStatus GetGameStatus() const;
     time_t GetGameTime() const;
 
     RenderedField RenderField() const;
+
+private:
+    std::vector<std::vector<ClosedCellType>> closed_cell_;
+    std::vector<std::vector<OpenCellType>> open_cell_;
+    std::vector<std::vector<int>> neighbour_;
+    time_t start_time_;
+    GameStatus game_status_ = GameStatus::NOT_STARTED;
+    int count_of_mines_;
+    int count_of_open_;
 };
