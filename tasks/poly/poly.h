2291ba0f4f2aedfd7d1ba9902597ead7383826de
Arina Shaydeman <adshaydeman_1@edu.hse.ru>
2023-03-07 09:52:41 +0300

    attempt1

--- tasks/poly/poly.h
+++ tasks/poly/poly.h
@@ -1,4 +1,27 @@
 #pragma once
 
+#include <vector>
+#include <iostream>
+#include <tuple>
+#include <map>
+
 class Poly {
+public:
+    Poly(){};
+    Poly(std::initializer_list<int64_t> list);
+    Poly(std::initializer_list<std::pair<int64_t, int64_t>> list);
+
+    int64_t operator()(int64_t val) const;
+    bool operator==(Poly poly) const;
+    Poly operator-(Poly poly) const;
+    Poly operator+(Poly poly) const;
+    void operator+=(Poly poly);
+    void operator-=(Poly poly);
+    Poly operator*(Poly poly) const;
+    Poly operator-() const;
+
+    friend std::ostream& operator<<(std::ostream& out, const Poly& poly);
+
+private:
+    std::map<size_t, int64_t> coef_;
 };
